dc54ced1e1e725e575a1d3246c4e7c3d558feda3
AdrenaRose <Charlenejparker81@gmail.com>
2023-04-24 22:14:33 -0400

    readMe final design

---
+++ readMe.md
@@ -0,0 +1,23 @@
+# Titles
+# Table of Contents  
+  1. [Description](#description)
+  2. [Installation](#installation)
+  3. [Usage](#usage)
+  4. [License](#license)
+  5. [Contributions](#contribution)
+  6. [Tests](#tests)
+  7. [Questions](#questions)<br>
+## Description<br>
+Description<br>
+## Installation<br>
+Installation<br>
+## Usage<br>
+Usage<br>
+## License<br>
+This application is covered under the MIT License.<br>
+## Contribution<br>
+Contribution<br>
+## Tests<br>
+## Questions<br>
+GitHub Profile: [AdrenaRose](https://www.github.com/AdrenaRose)<br>
+If you have any questions, please reach out to me at contact@gmail.com.
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -13,22 +13,29 @@ function renderLicenseSection(license) {}
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(answers) {
   console.log(answers);
-  return `# Title: ${answers.title}
+  return `# ${answers.title}
 # Table of Contents  
-  1. [Description](#Description:)
-  2. [Installation](#Installation_Instructions:)
-  3. [Usage](#Usage_Information:)
-  4. [License](#License:)
-  5. [Tests](#Tests:)
-  6. [Questions](#Questions:)<br>
-## Description: ${answers.description}<br>
-## Installation Instructions: ${answers.installation}<br>
-## Usage Information: ${answers.usage}<br>
-## License: ${answers.license}<br>
-## Contribution Guidelines: ${answers.contributions}<br>
-## Tests:<br>
-## Questions: [${answers.username}](https://www.github.com/${answers.username})<br>
-If you have any questions, please reach out to me at ${answers.email}.`
+  1. [Description](#description)
+  2. [Installation](#installation)
+  3. [Usage](#usage)
+  4. [License](#license)
+  5. [Contributions](#contribution)
+  6. [Tests](#tests)
+  7. [Questions](#questions)
+## Description<br>
+${answers.description}<br>
+## Installation<br>
+${answers.installation}<br>
+## Usage<br>
+${answers.usage}<br>
+## License<br>
+This application is covered under the ${answers.license}.<br>
+## Contribution<br>
+${answers.contributions}<br>
+## Tests<br>
+## Questions<br>
+GitHub Profile: [${answers.username}](https://www.github.com/${answers.username})<br>
+If you have any questions, please reach out to me at ${answers.email}.`;
 }
 
 module.exports = { generateMarkdown };
